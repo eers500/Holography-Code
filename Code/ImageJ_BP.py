2b5af8897761546f512c466c9fce2a878aada7d4
Erick Rodriguez Salas <eers500@its.york.ac.uk>
2019-01-17 14:17:10 +0000

    First master: BP filter

--- Code/ImageJ_BP.py
+++ Code/ImageJ_BP.py
@@ -23,7 +23,7 @@ else:
     axis = 'square'
 
 del ni,nj
-plt.figure(1)
+#plt.figure(1)
 #plt.subplot(221)
 plt.title('Sample Image')
 plt.imshow(img_gs,cmap='gray')
@@ -34,7 +34,7 @@ img_fft = np.fft.fftshift(imgfft)
 img_amp = abs(img_fft)
 del imgfft
 
-plt.figure(2)
+#plt.figure(2)
 #plt.subplot(222)
 plt.title('Fourier Transform')
 plt.imshow(np.log10(img_amp),cmap='gray')
@@ -58,8 +58,8 @@ for ii in range(0,ni-1):
         SCO[ii, jj] = np.exp(-((ii-MIS/2)**2+(jj-MIS/2)**2)*(2*xs/MIS)**2)
         
 BP = SCO - LCO
-plt.figure(3)
 
+#plt.figure(3)
 #plt.subplot(223)
 plt.title('Bandpass Filter')
 plt.imshow(BP,cmap='gray')        
@@ -76,7 +76,7 @@ elif axis == 'j':
     img_filt = img_filt[:,0:-dn]
     
 
-plt.figure(4)
+#plt.figure(4)
 #plt.subplot(224)
 plt.title('Filtered Image')
 plt.imshow(img_filt, cmap = 'gray')
