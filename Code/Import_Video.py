b3c883d99e080543425aa52fe3a94abce0afc921
Erick <eers500@york.ac.uk>
2019-01-25 20:27:05 +0000

    Import video as stack of images in a numpy array [x,y,frame]

--- Code/Import_Video.py
+++ Code/Import_Video.py
@@ -1,47 +1,29 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 24 14:27:39 2019
-
-@author: eers500
-"""
-
-import numpy as np
-import cv2
-
-cap = cv2.VideoCapture('MF1_30Hz_200us_awaysection.avi')
-cap.isOpened()
-
-count = 0
-success = True
-while success:
-    succes,image = cap.read()
-#%%
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+import time
 
 vidcap = cv2.VideoCapture("MF1_30Hz_200us_awaysection.avi")
 success,image = vidcap.read()
-#im = image[:,:,0]
-#a = im[:,:,0]
 num_frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
-#plt.imshow(a,cmap="gray")
-
 imStack = image[:,:,0]
+i = 1
 #imStack.reshape(())
-while success:
-    success,fr = vidcap.read()
-    frame = fr[:,:,0]
-    imStack = np.dstack((imStack,frame))
+#while success:    
+#    success,fr = vidcap.read()
+#    frame = fr[:,:,0]
+#    imStack = np.dstack((imStack,frame))
+#    i = i+1
 
-
+t0 = time.time()
+for ii in range(1,num_frames):
+        success,fr = vidcap.read()
+        frame = fr[:,:,0]
+        imStack = np.dstack((imStack,frame))
+t1 = time.time()
+time = t1-t0        
 #%%
-#print(vidcap.read())
-success,image = vidcap.read()
-count = 0
-success = True
-while success:
-  success,image = vidcap.read()
-  print ('Read a new frame: ', success)
-  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
-  count += 1
+for i in range(imStack.shape[2]):
+    plt.imshow(imStack[:,:,i])
+    plt.pause(0.01)
