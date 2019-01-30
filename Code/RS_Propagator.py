33a653449aa64294df4e12625ca7918621c4cbdf
Erick Rodriguez Salas <eers500@its.york.ac.uk>
2019-01-30 16:08:36 +0000

    RS propagator and symbolic work

--- Code/RS_Propagator.py
+++ Code/RS_Propagator.py
@@ -1,4 +1,53 @@
-# -*- coding: utf-8 -*-
+#%% -*- coding: utf-8 -*-
 # Rayleigh-Sommerfeld Back-Propagator
+import math as m
+import matplotlib.image as mpimg
+from scipy import signal
+import numpy as np
+
+img = mpimg.imread('MF1_30Hz_200us_away_median.png')
+
+#%% Normalize with median image
+imgFILT = signal.medfilt(img, kernel_size = 3)
+hologram = imgFILT-1   
+                      # b(r)-1
+size = np.shape(hologram)
+nx = size[0]
+ny = size[1]
+
+#%%
+lambdaa = 0.525                                  # Wavelength of field
+nm = 1                                           # Refraction index of medium
+k = 2*math.pi/lambdaa                                # wavenumber 
+
+x = np.arange(nx)
+y = np.arange(ny)
+z = np.arange(0,10)
+
+xx,yy = np.meshgrid(x,y)
+r = (xx**2+yy**2)**(1/2)
+
+#%% Simbolic expresion for A (differential in propagator)
+from sympy import *
+
+A = z*m.exp(1j*k*(r**2+z**2)**(1/2))*(1j*k*(r**2+z**2)**(-1)-(r**2+z**2)**(-3/2))
+
+x1,y1,z1 = symbols('x1 y1 z1')
+A = 
 
 
+for kk in range(len(z)):
+    
+ 
+
+
+
+
+#%% 
+#import matplotlib.pyplot as plt
+#
+#plt.figure(1)
+#plt.imshow(imgFILT, cmap = 'gray')
+#
+#plt.figure(2)
+#plt.imshow(imgFILTs, cmap = 'gray')
--- Code/symbolicComputing.py
+++ Code/symbolicComputing.py
@@ -1,14 +1,20 @@
-# -*- coding: utf-8 -*-
-from sympy import *
-x, y, z = symbols('x y z')
-f = x+y+z
-a = float(f.subs([(x,1), (y,1), (z,1)]))
-#%%
-from sympy import *
+#%% Symbolic computing and 3D plot (surface)
 import numpy as np
-x, y = symbols('x y')
-f = x+y
-xx = np.arange(0,1,0.1)
-yy = xx
+from sympy import *
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-a = np.array(f.subs([(x,xx),(y,yy)]))
+x, y, z = symbols('x y z')
+x1 = np.arange(-3,3,1)
+y1 = x1
+
+xx, yy = np.meshgrid(x1,y1)
+
+expr = z*exp(-(x**2+y**2))
+
+f = lambdify([x,y], expr,'numpy')
+c = f(xx, yy,)
+
+fig = plt.figure()
+ax = fig.gca(projection = '3d')
+ax.plot_surface(xx, yy, c, cmap = 'jet')
