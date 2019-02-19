994afd303b70fe615025a44eb9ec09a138a7a411
Erick Rodriguez Salas <eers500@its.york.ac.uk>
2019-02-19 14:51:56 +0000

    RS_Propagator3.py done. Plot image scalng problem.

--- Code/RS_Propagator3.py
+++ Code/RS_Propagator3.py
@@ -4,10 +4,10 @@ import math as m
 import matplotlib.image as mpimg
 from scipy import signal
 import numpy as np
-import scipy
-from scipy import ndimage
+import matplotlib.pyplot as plt
+import time
 
-
+t0 = time.time()
 I = mpimg.imread('131118-1.png')
 #img = mpimg.imread('MF1_30Hz_200us_away_median.png')
 #%% Median image
@@ -35,8 +35,27 @@ z = np.arange(1,21)
 
 #%%
 #qsq = ((lambdaa/(N*n))*nx)**2 + ((lambdaa/(N*n))*ny)**2
-P = np.empty_like(IB)
+P = np.empty_like(IB, dtype = complex)
 for i in range(N):
     for j in range(N):
 #        print(i,j)
         P[i,j] = ((lambdaa*fs)/(N*n))**2*((i-N/2)**2+(j-N/2)**2)
+        
+Q = np.sqrt(1-P)-1
+
+R = np.empty([N,N,z.shape[0]], dtype = complex)
+Iz = R
+for k in range(z.shape[0]):
+    for i in range(N):
+        for j in range(N):
+            R[i,j,k] = np.exp((-1j*k*z[k])*Q[i,j])
+    Iz[:,:,k] = 1+np.fft.ifft2(BP*FT*R[:,:,k])
+        
+Iz = np.real(Iz)
+Im = 50*(Iz - 1) + 128
+
+t = time.time() - t0           
+
+plt.imshow(Im[:,:,19], cmap = 'gray')
+
+
