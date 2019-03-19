f8cc4ae9f80948af8d21e0c925d03e60c1fb4fb9
Erick Rodriguez Salas <eers500@its.york.ac.uk>
2019-03-19 15:23:34 +0000

    ZGradientStackPositions.py calculate loctions from gradient stack in a list LOCS and exports it to locs.txt

--- Code/ZGradientStackPositions.py
+++ Code/ZGradientStackPositions.py
@@ -5,6 +5,8 @@ import numpy as np
 import functions as f
 import tkinter as tk
 import tkinter.filedialog
+from skimage.feature import peak_local_max
+
 
 tk.Tk().withdraw()
 PATH = tk.filedialog.askopenfilename()
@@ -19,7 +21,29 @@ I = VID[:, :, 0]
 IM = f.rayleighSommerfeldPropagator(I, I_MEDIAN, Z)
 GS = f.zGradientStack(I, I_MEDIAN, Z)
 
+#%% From Labview code
+from functions import videoImport
+GS = videoImport('131118-1_(frame0)gradient.avi')
+
+#%% Find (x,y,z) of cells
+LOCS = np.zeros((1,3))
+for k in range(GS.shape[2]):
+   PEAKS = peak_local_max(GS[:, :, k], indices=True)
+   ZZ = np.ones((PEAKS.shape[0],1))*k
+   PEAKS = np.append(PEAKS, ZZ, axis=1)
+   LOCS = np.append(LOCS, PEAKS, axis=0)
+LOCS = np.delete(LOCS, 0, 0)   
+
+np.savetxt('locs.txt', LOCS)    
 #%%
-GS50 = GS[:, :, 50]
-GRAD = np.gradient(GS50)
-GRADasb = np.sqrt(GRAD[0]**2+GRAD[1]**2)
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import pyplot
+
+fig = pyplot.figure()
+ax = Axes3D(fig)
+
+ax.scatter(LOCS[:, 0], LOCS[:, 1], LOCS[:, 2])
+pyplot.show()
+    
+    
+    
