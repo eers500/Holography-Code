9de1070435d0d04d34f65b1444a33164cd6ec2c1
Erick Rodriguez Salas <eers500@its.york.ac.uk>
2019-04-18 15:43:19 +0100

    Update Holography.py

--- Code/Holography.py
+++ Code/Holography.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
-import numpy as np
-#import matplotlib.image as mpimg
-#import time
-import functions as f
+#%% 
+import time
 import tkinter as tk
 import tkinter.filedialog
+import numpy as np
+#import matplotlib.image as mpimg
+import functions as f
+
 
 tk.Tk().withdraw()
 PATH = tk.filedialog.askopenfilename()
@@ -16,19 +18,19 @@ Z = 0.02*np.arange(1, 151)
 
 #%% Batch calculation
 FILENAME = "frameStack_frame"
+T0 = time.time()
+T = np.empty(3)
 for i in range(3):
 #for i in range(VID.shape[2]):
     FILE = FILENAME + str(i)
     IM = f.rayleighSommerfeldPropagator(VID[:, :, i], I_MEDIAN, Z)
     f.exportAVI(FILE+".avi", IM, IM.shape[0], IM.shape[1], 24)
+    T[i] = time.time() - T0
+    print(T[i])
 #%%
-I = VID[:, :, 0]
-IM = f.rayleighSommerfeldPropagator(I, I_MEDIAN, Z)
-GS = f.zGradientStack(I, I_MEDIAN, Z)
-
-
-f.exportAVI('frameStack.avi',IM, IM.shape[0], IM.shape[1], 24)
-
-
-VI = f.videoImport('frameStack.avi')
-
+#I = VID[:, :, 0]
+#IM = f.rayleighSommerfeldPropagator(I, I_MEDIAN, Z)
+#GS = f.zGradientStack(I, I_MEDIAN, Z)
+#f.exportAVI('frameStack.avi', IM, IM.shape[0], IM.shape[1], 24)
+#
+#VI = f.videoImport('frameStack.avi')
