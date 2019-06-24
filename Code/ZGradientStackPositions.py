42d16577eb995de69c5d355b272e3214ed25a896
eers500 <45375647+eers500@users.noreply.github.com>
2019-06-24 18:48:49 +0100

    ZGradientStack.py Bokeh
    Plot 2D plot with Bokeh

--- Code/ZGradientStackPositions.py
+++ Code/ZGradientStackPositions.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+##
 import numpy as np
-#import matplotlib.image as mpimg
+# import matplotlib.image as mpimg
 import time
 import functions as f
 import tkinter as tk
@@ -9,40 +10,49 @@ from skimage.feature import peak_local_max
 
 T0 = time.time()
 tk.Tk().withdraw()
-PATH = tk.filedialog.askopenfilename(title = "Select file",filetypes = (("avi files","*.avi"),("all files","*.*")))
+PATH = tk.filedialog.askopenfilename(title="Select file", filetypes=(("avi files", "*.avi"), ("all files", "*.*")))
 
 VID = f.videoImport(PATH)
 I_MEDIAN = f.medianImage(VID)
-#I = mpimg.imread('131118-1.png')
-Z =0.2*np.arange(1, 151)
+# I = mpimg.imread('131118-1.png')
+Z = 0.2 * np.arange(1, 151)
 THRESHOLD = 0.3
-#%%
-I = VID[:, :, 0]
-#IM = f.rayleighSommerfeldPropagator(I, I_MEDIAN, Z)
-GS, IM = f.zGradientStack(I, I_MEDIAN, Z)    # GradientStack and RS propagator
-GS[GS<THRESHOLD] = 0
-#%% From Labview code
-#from functions import videoImport
-#GS = videoImport('131118-1_(frame0)gradient.avi')
 
-#%% Find (x,y,z) of cells
-LOCS = np.zeros((1,3))
+##
+I = VID[:, :, 0]
+# IM = f.rayleighSommerfeldPropagator(I, I_MEDIAN, Z)
+GS, IM = f.zGradientStack(I, I_MEDIAN, Z)  # GradientStack and RS propagator
+GS[GS < THRESHOLD] = 0
+
+## From Labview code
+# from functions import videoImport
+# GS = videoImport('131118-1_(frame0)gradient.avi')
+
+## Find (x,y,z) of cells
+LOCS = np.zeros((1, 3))
 for k in range(GS.shape[2]):
-   PEAKS = peak_local_max(GS[:, :, k], indices=True)   #Check for peak radius
-   ZZ = np.ones((PEAKS.shape[0],1))*k
+   PEAKS = peak_local_max(GS[:, :, k], indices=True)  # Check for peak radius
+   ZZ = np.ones((PEAKS.shape[0], 1)) * k
    PEAKS = np.append(PEAKS, ZZ, axis=1)
    LOCS = np.append(LOCS, PEAKS, axis=0)
-LOCS = np.delete(LOCS, 0, 0)   
+LOCS = np.delete(LOCS, 0, 0)
 
-np.savetxt('locs.txt', LOCS)    
-print(time.time()-T0)
-#%%
+np.savetxt('locs.txt', LOCS)
+print(time.time() - T0)
+## 3D Scatter Plot
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import pyplot
 
 fig = pyplot.figure()
 ax = Axes3D(fig)
 
-ax.scatter(LOCS[:, 0], LOCS[:, 1], LOCS[:, 2])
+ax.scatter(LOCS[:, 0], LOCS[:, 1], LOCS[:, 2], s=50, marker='o')
+ax.tick_params(axis='both', labelsize=10)
+ax.set_title('Cells Positions in 3D', fontsize='20')
+ax.set_xlabel('x (pixels)', fontsize='18')
+ax.set_ylabel('y (pixels)', fontsize='18')
+ax.set_zlabel('z (slices)', fontsize='18')
 pyplot.show()
-    
+
+
+
--- Code/rs_propagator_grier.py
+++ Code/rs_propagator_grier.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#%% Rayleigh-Sommerfeld Back-Propagator
+## Rayleigh-Sommerfeld Back-Propagator
 import math as m
 import time
 import matplotlib.image as mpimg
@@ -9,19 +9,20 @@ from functions import bandpassFilter, exportAVI
 from progress.bar import Bar
 
 T0 = time.time()
-#I = mpimg.imread('131118-1.png')
+# I = mpimg.imread('131118-1.png')
 I = mpimg.imread('MF1_30Hz_200us_awaysection.png')
-#I = np.uint8(I/np.max(I)*255)
-#%% Median image
-#IB = mpimg.imread('AVG_131118-2.png')
+# I = np.uint8(I/np.max(I)*255)
+
+## Median image
+# IB = mpimg.imread('AVG_131118-2.png')
 IB = mpimg.imread('AVG_MF1_30Hz_200us_awaysection.png')
-#IB = np.uint8(IB/np.max(IB)*255)
-#IB = mpimg.imread('img_gs.png')
-#IB = signal.medfilt2d(I, kernel_size = 3)
-#IZ = np.where(IB == 0)
+# IB = np.uint8(IB/np.max(IB)*255)
+# IB = mpimg.imread('img_gs.png')
+# IB = signal.medfilt2d(I, kernel_size = 3)
+# IZ = np.where(IB == 0)
 IB[IB == 0] = np.average(IB)
-#IN = I/IB     #divide
-IN = I - IB    #substract as in imageJ
+IN = I/IB     #divide
+# IN = I - IB    #substract as in imageJ
 IN[IN < 0] = 0
 
 N = 1.3226
@@ -34,8 +35,9 @@ K = 2*m.pi*N/LAMBDA      #Wavenumber
 
 _, BP = bandpassFilter(IN, 2, 30)
 E = BP*np.fft.fft2(IN - 1)
-#%%
-#qsq = ((lambdaa/(N*n))*nx)**2 + ((lambdaa/(N*n))*ny)**2
+
+##
+# qsq = ((lambdaa/(N*n))*nx)**2 + ((lambdaa/(N*n))*ny)**2
 P = np.empty_like(IB, dtype=complex)
 for i in range(NI):
     for j in range(NJ):
@@ -52,19 +54,20 @@ for k in range(Z.shape[0]):
     IZ[:, :, k] = 1 + np.real(np.fft.ifft2(E*R[:, :, k]))
     BAR.next()
 BAR.finish()
-#    print(('RS', k))
+# print(('RS', k))
 IZ8 = np.uint8((IZ - np.min(IZ))*255)
 IZ8 = np.uint8(IZ8/np.max(IZ8)*255)
 IZ8 = np.uint8(255*(IZ8/255)**2)
-#IM = 50*(IZ - 1) + 128
-#IMM = IM/np.max(IM)*255
-#IMM = np.uint8(IMM)
+# IM = 50*(IZ - 1) + 128
+# IMM = IM/np.max(IM)*255
+# IMM = np.uint8(IMM)
 
 print(time.time() - T0)
-#%%
-#plt.imshow(IZ[:, :, 149], cmap = 'gray')
 
-#%%
-#IZZ = (IZ-np.min(IZ))/np.max((IZ-np.min(IZ)))*255
-#IZZZ = np.uint8(IZZ)
-#exportAVI('IZZ.avi',IZZ, IZZ.shape[0], IZZ.shape[1], 24)
+##
+# plt.imshow(IZ[:, :, 149], cmap = 'gray')
+
+##
+# IZZ = (IZ-np.min(IZ))/np.max((IZ-np.min(IZ)))*255
+# IZZZ = np.uint8(IZZ)
+# exportAVI('IZZ.avi',IZZ, IZZ.shape[0], IZZ.shape[1], 24)
