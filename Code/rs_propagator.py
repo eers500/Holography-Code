0cda06a73935a8c0504367562c6e777554c2bd23
eers500 <45375647+eers500@users.noreply.github.com>
2019-08-09 18:43:29 +0100

    rs_propagator_grier.py shifted bandapass filter

--- Code/rs_propagator.py
+++ Code/rs_propagator.py
@@ -26,36 +26,20 @@ IN = I/IB   #divide
 # IN[IN < 0] = 0
 
 #%%
-N = 1.3226
-# LAMBDA = 0.642           #HeNe
-# MPP = 2              # Magnification: 10x, 20x, 50x, etc
-# FS = 0.711                #Sampling Frequency px/um
-NI = np.shape(IN)[0]
-NJ = np.shape(IN)[1]
-# Z = FS/2*np.arange(1, 151)
-# K = 2*m.pi*N/LAMBDA      #Wavenumber
-
-# IBAND, BP = bandpassFilter(IN, 2, 30)
-# E = BP*np.fft.fft2(IN - 1)
-
-#%%
 # Hologram dimensions
 SZ = np.shape(IN)
 NX = SZ[0]
 NY = SZ[1]
 
 #%%
-# Volumetric slices
-# nz = len(Z)
-
-#%%
 # Parameters
-LAMBDA = 0.642
-MPP = 0.711
-CI = 1j
+N = 1.3226
+LAMBDA = 0.642/N
+MPP = 0.711/20
 K = 2*m.pi*MPP/LAMBDA
-Z = np.arange(1, 151)/10
-nz = len(Z)
+# Z = np.arange(1, 151)/MPP*3
+Z = np.linspace(1, 150, 150)
+NZ = len(Z)
 
 #%%
 # Phase factor for Rayleigh-Sommerfeld propagator in Fourier space
@@ -66,21 +50,22 @@ QSQ = QX**2
 QY = np.arange(NY) - NY/2
 QY *= LAMBDA / (NY*MPP)
 
-A = np.ones((NY))
+A = np.ones((NY,))
 B = np. ones((NX,))
 QSQ = np.matmul(np.asmatrix(QSQ).T, np.asmatrix(A)) + np.matmul(np.asmatrix(B).T, np.asmatrix(QY**2))
 QSQ = np.asarray(QSQ)
 
 QFACTOR = K*np.sqrt(1-QSQ).astype(complex)
-IKAPPA = CI * np.real(QFACTOR)
+IKAPPA = 1j * np.real(QFACTOR)
 GAMMA = np.imag(QFACTOR)
 
 IBAND, BP = bandpassFilter(IN, 2, 30)
+# BP = np.ones_like(BP)
 E = np.fft.fftshift(BP)*np.fft.fftshift(np.fft.fft2(IN - 1))
 RES = np.empty([NX, NY, Z.shape[0]], dtype=complex)
 THISE = RES
 
-for j in range(Z.shape[0]):
+for j in range(NZ-1):
     Hqz = np.exp(IKAPPA * Z[j] - GAMMA * abs(Z[j]))
     THISE = E * Hqz
     THISE = np.fft.ifft2(np.fft.ifftshift(THISE))
@@ -88,11 +73,12 @@ for j in range(Z.shape[0]):
 
 #%%
 IZ = np.real(RES)
-
+IM = 50*(IZ - 1) + 128
 print(time.time() - T0)
 
 #%%
 # IZZ = np.abs(IZ)**2
 IZZ = (IZ-np.min(IZ))/np.max((IZ-np.min(IZ)))*255
+# IZZ = (IM-np.min(IM))/np.max((IM-np.min(IM)))*255
 IZZZ = np.uint8(IZZ)
-exportAVI('IZZZ.avi',IZZZ, IZZ.shape[0], IZZ.shape[1], 30)
+exportAVI('IZZZ.avi',255-IZZZ, IZZ.shape[0], IZZ.shape[1], 30)
--- Code/rs_propagator_grier.py
+++ Code/rs_propagator_grier.py
@@ -27,16 +27,17 @@ IN = I/IB   #divide
 # IN[IN < 0] = 0
 
 N = 1.3226
-LAMBDA = 0.642           #HeNe
+LAMBDA = 0.642/N           #HeNe
 # MPP = 2              # Magnification: 10x, 20x, 50x, etc
-FS = 0.711                #Sampling Frequency px/um
+FS = 0.711*2                #Sampling Frequency px/um
 NI = np.shape(IN)[0]
 NJ = np.shape(IN)[1]
-Z = FS/2*np.arange(1, 151)
+Z = np.arange(1, 151)
 K = 2*m.pi*N/LAMBDA      #Wavenumber
 
 IBAND, BP = bandpassFilter(IN, 2, 30)
-E = BP*np.fft.fft2(IN - 1)
+# E = BP*np.fft.fft2(IN - 1)
+E = np.fft.fftshift(BP)*np.fft.fftshift(np.fft.fft2(IN - 1))
 
 #%%
 # qsq = ((lambdaa/(N*n))*nx)**2 + ((lambdaa/(N*n))*ny)**2
@@ -53,19 +54,10 @@ if all(Z>0):
 R = np.empty([NI, NJ, Z.shape[0]], dtype=complex)
 IZ = np.empty_like(R, dtype=float)
 
-BAR = Bar('Processing', max=Z.shape[0])
 for k in range(Z.shape[0]):
     R[:, :, k] = np.exp((-1j*K*Z[k]*Q))
-    IZ[:, :, k] = 1 + np.real(np.fft.ifft2(E*R[:, :, k]))
-    BAR.next()
-BAR.finish()
-# print(('RS', k))
-IZ8 = np.uint8((IZ - np.min(IZ))*255)
-IZ8 = np.uint8(IZ8/np.max(IZ8)*255)
-IZ8 = np.uint8(255*(IZ8/255)**2)
-# IM = 50*(IZ - 1) + 128
-# IMM = IM/np.max(IM)*255
-# IMM = np.uint8(IMM)
+    # IZ[:, :, k] = 1 + np.real(np.fft.ifft2(E*R[:, :, k]))
+    IZ[:, :, k] = 1 + np.real(np.fft.ifft2(np.fft.ifftshift(E*R[:, :, k])))
 
 print(time.time() - T0)
 
@@ -73,6 +65,7 @@ print(time.time() - T0)
 # plt.imshow(IZ[:, :, 149], cmap = 'gray')
 
 #%%
+IM = 50*(IZ - 1) + 128
 IZZ = np.abs(IZ)**2
 IZZ = (IZZ-np.min(IZZ))/np.max((IZZ-np.min(IZZ)))*255
 IZZZ = np.uint8(255-IZZ)
