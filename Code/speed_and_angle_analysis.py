2fe49ae950921c34d6c6491c6e91c0de83993211
eers500 <45375647+eers500@users.noreply.github.com>
2022-09-21 19:05:06 -0500

    thesis update to run in York

--- Code/speed_and_angle_analysis.py
+++ Code/speed_and_angle_analysis.py
@@ -93,85 +93,90 @@ plot(fig)
 
 #%% Re orientation Angle with RDP
 
-def get_angles_rdp(sd, epsilon, degrees):
+def get_angles_rdp(sd, epsilon, angle_threshold):
     """ Re-orientation event angle"""
     
     from rdp import rdp
+    from scipy.signal import find_peaks
     
-    # ss = sd.values
-    # x = ss[:, :3]
-    # t = ss[:, 3]
-    # p = ss[0, 4]
+    epsilon = 0.5
     
     x = sd[['X', 'Y', 'Z']].values
     t = sd['TIME'].values
     p = sd['PARTICLE'].values[0]
     
-    xyz = rdp(x, epsilon)   # RDP reconstruction
-    xyz = np.hstack((xyz, p*np.ones((len(xyz), 1))))
+    coords = rdp(x, epsilon, return_mask=True)   # RDP reconstruction
+    xyz = np.hstack((x[coords, :], p*np.ones((np.count_nonzero(coords), 1))))
+    xyz = np.hstack((xyz, np.expand_dims(np.where(coords==True)[0], axis=1)))
     
     
     cos = np.ones(len(xyz))
     Dr = np.ones(len(xyz))
     for i in range(1, len(xyz)-1):
-        a1 = xyz[i-1, :]
-        a2 = xyz[i, :]
-        a3 = xyz[i+1, :]
+        a1 = xyz[i-1, :3]
+        a2 = xyz[i, :3]
+        a3 = xyz[i+1, :3]
         d21 = a2 - a1
         d32 = a3 - a2
         cos[i] = np.dot(d21, d32) / (np.sqrt(np.sum(d21**2)) * np.sqrt(np.sum(d32**2)))
         
-    if degrees:
-        angles = np.arccos(cos) * 180 / np.pi
-        
-    else:
-        angles = np.arccos(cos)
+    angles = np.arccos(cos) * 180 / np.pi
+    
+    
+    pks, _ = find_peaks(angles, height=angle_threshold)
+    ids = xyz[pks, -1].astype('int')                          # indices of original array
+    
+    
     
     # plt.figure()
-    # plt.plot(angle)
+    # plt.plot(t[coords], angles)
+    # plt.plot(t[ids], angles[pks], 'ro')
     
     # plt.figure()
-    # plt.hist(angle, 30)
+    # plt.hist(angles, 30)
     
     # plt.figure(figsize=(9, 4.5))
     # ax0 = plt.subplot2grid((6, 6), (0, 0), 6, 3, projection='3d')
     # ax0.set_facecolor('none')
     # ax0.plot(sd.X, sd.Y, sd.Z, 'b-', label='original data')
-    # # ax0.plot(xyz[:, 0], xyz[:, 1], xyz[:, 2], 'r.', label='Turns')
+    # ax0.plot(sd.X[ids], sd.Y[ids], sd.Z[ids], 'ro', label='original data')
     # ax0.set_title('Smoothed track')
 
     # ax1 = plt.subplot2grid((6, 6), (0, 3), 6, 3, projection='3d')
     # ax1.set_facecolor('none')
-    # ax1.plot(xyz[:, 0], xyz[:, 1], xyz[:, 2], 'b-', label='RDP reconstructed eps = '+np.str(eps3d))
-    # ax1.plot(xyz[:, 0], xyz[:, 1], xyz[:, 2], 'r.', label='Turns')
+    # ax1.plot(xyz[:, 0], xyz[:, 1], xyz[:, 2], 'b-', label='RDP reconstructed eps = '+np.str(epsilon))
+    # ax1.plot(sd.X[ids], sd.Y[ids], sd.Z[ids], 'ro', label='Turns')
     # ax1.set_title('RDP Simplification')
     # ax1.legend(loc='best')
     
-    angles = np.hstack((angles.reshape((len(angles), 1)), p*np.ones((len(angles), 1))))
+    # angles = np.hstack((angles.reshape((len(angles), 1)), p*np.ones((len(angles), 1))))
     
-    return angles[1:-1], xyz
+    return ids, xyz, angles       # index of turns of original array, RDP simplification, turn angles
 
-eps = 0.6
+
+
+eps = 0.5
 pnum = tracks_w_speed['PARTICLE'].unique()
 
+indices = []
 angles = []
 recs = []
 for pn in tqdm(pnum):
     sd = tracks_w_speed[tracks_w_speed['PARTICLE'] == pn]
-    a, rec = get_angles_rdp(sd, eps, True)
+    i, r, a = get_angles_rdp(sd, eps, 45)
+    indices.append(i)
     angles.append(a)
-    recs.append(rec)
+    recs.append(r)
     
 angles = np.concatenate(angles)
 recs = np.concatenate(recs)
 
-angles = pd.DataFrame(angles, columns=['ANGLES', 'PARTICLE'])
-recs= pd.DataFrame(recs, columns=['X', 'Y', 'Z', 'PARTICLE'])
-amean = angles.ANGLES.mean()
+recs= pd.DataFrame(recs, columns=['X', 'Y', 'Z', 'PARTICLE', 'INDEX_ORIGIN'])
+amean = angles.mean()
 
 #%%
-plt.hist(angles['ANGLES'], 30)
-plt.vlines(amean, ymin=0, ymax=50, colors='red')
+m = plt.hist(angles, 32)[0]
+plt.vlines(amean, ymin=0, ymax=m.max(), colors='red')
 plt.title('Mean: '+ str(round(amean, 3)))
 plt.grid()
 
@@ -183,7 +188,7 @@ from matplotlib import pyplot
 fig = pyplot.figure()
 ax = Axes3D(fig)
 
-pn = 9
+pn = 5
 
 sd =  tracks_w_speed[tracks_w_speed['PARTICLE'] == pn]
 rec = recs[recs['PARTICLE'] == pn]
@@ -203,7 +208,10 @@ pyplot.show()
 
 
 #%% With LW metric
-def xi_metric(sd):
+def xi_metric(sd, threshold):
+    
+    from scipy.signal import find_peaks
+    
     x = sd[['X', 'Y', 'Z']].values
     v = sd['SPEED'].values
     t = sd['TIME'].values
@@ -236,32 +244,36 @@ def xi_metric(sd):
         # psi[i] = const * np.abs(np.dot(d21, d32)) / (t[i+1] - t[i-1])
         
         xi[i] = const * arcdot[i] / (t[i+1] - t[i-1])
-        
+    
     angles = np.arccos(cos) * 180 / np.pi    
+    angles[angles > 45] = 0
+    
+    pks, _ = find_peaks(xi, height=threshold)
 
-    return xi[1:-1], angles[1:-1]
+    # plt.plot(t, xi)
+    # plt.plot(t[pks], xi[pks], 'ro')
+    
+
+    return pks, xi, angles
 
 
 # Test
 pn = 9
 sd = tracks_w_speed[tracks_w_speed['PARTICLE'] == pn]
 
-psi, angles = xi_metric(sd)
+pks, xi, angles = xi_metric(sd, 40)
 
-# Psi and angle plot
 plt.figure()
-plt.subplot(2,1,1)
-plt.plot(sd['TIME'].values[1:-1],psi)
+plt.hist(angles)
+
+
+# Xi plot
+plt.figure()
+plt.plot(sd['TIME'], xi)
+plt.plot(sd['TIME'].values[pks], xi[pks], 'ro')
 # plt.xlabel('Time (s)', fontsize=10)
 plt.ylabel('$\Xi$ (degrees $s^{-1}$)', fontsize=10)
 plt.title('Archea Track', fontsize=10)
-plt.grid()
-
-plt.subplot(2,1,2)
-plt.plot(sd['TIME'].values[1:-1], angles)
-plt.xlabel('Time (s)', fontsize=10)
-plt.ylabel('Angles (degrees)', fontsize=10)
-# plt.title('Angles', fontsize=10)
 plt.grid()
 
 
@@ -269,62 +281,9 @@ plt.grid()
 fig = pyplot.figure()
 ax = Axes3D(fig)
 
-sd =  tracks_w_speed[tracks_w_speed['PARTICLE'] == pn]
-rec = recs[recs['PARTICLE'] == pn]
 
 ax.plot(sd.X, sd.Y, sd.Z, linewidth=3)
-# ax.scatter(rec.X, rec.Y, rec.Z, s=50, marker='o', c='red')
-
-
-ax.tick_params(axis='both', labelsize=10)
-ax.set_title('Cells Positions in 3D', fontsize='20')
-ax.set_xlabel('x (um)', fontsize='18')
-ax.set_ylabel('y (um)', fontsize='18')
-ax.set_zlabel('z (um)', fontsize='18')
-# fig.colorbar(p, ax=ax)
-pyplot.show()
-
-#%% Test with LW track
-file = '/media/erick/NuevoVol/LINUX_LAP/PhD/Thesis/Results/1/Archea/LW track with reversals/track243_1_0.txt'
-sd_lw = np.loadtxt(file)
-sd_lw = pd.DataFrame(sd_lw[:,:4], columns=['TIME', 'X' ,'Y', 'Z'])
-# sd_lw = pd.DataFrame(sd_lw[:,[0, 4, 5, 6]], columns=['TIME', 'X' ,'Y', 'Z'])
-
-sd_lw_speed = np.array(f.get_speed(sd_lw)).transpose()
-sd_lw_speed = pd.DataFrame(sd_lw_speed, columns=['SPEED', 'X', 'Y', 'Z', 'TIME'])
-
-
-# LW metric
-psi, angles = xi_metric(sd)
-
-plt.figure()
-plt.subplot(2,1,1)
-plt.plot(sd['TIME'].values[1:-1],psi)
-# plt.xlabel('Time (s)', fontsize=10)
-plt.ylabel('$\Xi$ ($\mu m^{2}s^{-1}$)', fontsize=10)
-plt.title('LW Track', fontsize=10)
-plt.grid()
-
-plt.subplot(2,1,2)
-plt.plot(sd['TIME'].values[1:-1], angles)
-plt.xlabel('Time (s)', fontsize=10)
-plt.ylabel('Angles (degrees)', fontsize=10)
-# plt.title('Angles', fontsize=10)
-plt.grid()
-
-#%% RPD
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import pyplot
-
-sd_lw_speed['PARTICLE'] = np.ones(len(sd_lw_speed))
-angles, rec = get_angles_rdp(sd_lw_speed, 10, True)
-
-fig = pyplot.figure()
-ax = Axes3D(fig)
-
-
-ax.plot(sd_lw_speed.X, sd_lw_speed.Y, sd_lw_speed.Z, linewidth=3)
-ax.scatter(rec[:, 0], rec[:, 1], rec[:, 2], s=50, marker='o', c='red')
+ax.scatter(sd.X.values[pks[-1]], sd.Y.values[pks[-1]], sd.Z.values[pks[-1]], s=50, marker='o', c='red')
 
 
 ax.tick_params(axis='both', labelsize=10)
@@ -336,25 +295,72 @@ ax.set_zlabel('z (um)', fontsize='18')
 pyplot.show()
 
 #%%
-pks, _ = find_peaks(angles, height=1)
-plt.plot(t[2:-2], angles[1:-1])
-plt.plot(t[1:-1][pks], angles[pks], 'o')
+X = sd[['X', 'Y', 'Z']].values
+peak = pks[-1]
+
+theta = []
+
+
+for d in range(1, 200):
+    a21 = X[peak, :] - X[peak-d, :]
+    a32 = X[peak+d, :] - X[peak, :]
     
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import pyplot
+    norm21 = np.sqrt(np.dot(a21, a21))
+    norm32 = np.sqrt(np.dot(a32, a32))
+    
+    th = np.arccos(np.dot(a21, a32) / (norm21*norm32)) * 180 / np.pi
+    # print(theta)
+    theta.append(th)
+
+plt.plot(range(1, 200), theta)
 
 
-fig = pyplot.figure()
+
+#%%
+#######################################################################################################
+###
+###                                     Test with LW track
+###
+#######################################################################################################
+
+file = '/media/erick/NuevoVol/LINUX_LAP/PhD/Thesis/Results/1/Archea/LW track with reversals/track243_1_0.txt'
+sd_lw = np.loadtxt(file)
+sd_lw = pd.DataFrame(sd_lw[:,:4], columns=['TIME', 'X' ,'Y', 'Z'])
+sd_lw['FRAME'] = np.arange(len(sd_lw))
+
+sd_lw_smooth = np.array(f.csaps_smoothing(sd_lw, 0.5, False, 6)).transpose()
+sd_lw = pd.DataFrame(sd_lw_smooth, columns=['X', 'Y', 'Z', 'TIME'])
+
+sd_lw_speed = np.array(f.get_speed(sd_lw)).transpose()
+sd_lw_speed = pd.DataFrame(sd_lw_speed, columns=['SPEED', 'X', 'Y', 'Z', 'TIME'])
+
+
+# LW metric
+pks, xi, angles = xi_metric(sd_lw_speed, 160)
+
+plt.figure()
+plt.subplot(2,1,1)
+plt.plot(sd_lw_speed['TIME'].values, xi)
+plt.plot(sd_lw_speed['TIME'].values[pks], xi[pks], 'ro')
+# plt.xlabel('Time (s)', fontsize=10)
+plt.ylabel('$\Xi$ ($\mu m^{2}s^{-1}$)', fontsize=10)
+plt.title('LW Track', fontsize=10)
+plt.grid()
+
+plt.subplot(2,1,2)
+plt.plot(sd_lw_speed['TIME'].values, angles)
+plt.plot(sd_lw_speed['TIME'].values[pks], angles[pks], 'ro')
+plt.xlabel('Time (s)', fontsize=10)
+plt.ylabel('Angles (degrees)', fontsize=10)
+# plt.title('Angles', fontsize=10)
+plt.grid()
+
+##
+fig = plt.figure()
 ax = Axes3D(fig)
 
-# pn = pnum[1]
-# pn = 9
-
-# sd =  tracks_w_speed[tracks_w_speed['PARTICLE'] == pn]
-# rec = recs[recs['PARTICLE'] == pn]
-
-p = ax.scatter(sss.X, sss.Y, sss.Z, linewidth=3, c=sss.TIME)
-ax.scatter(x[1:-1][pks, 0], x[1:-1][pks, 1], x[1:-1][pks, 2], s=500, marker='o', c='red')
+ax.plot(sd_lw_speed.X, sd_lw_speed.Y, sd_lw_speed.Z, linewidth=3)
+ax.scatter(sd_lw_speed.X.values[pks], sd_lw_speed.Y.values[pks], sd_lw_speed.Z.values[pks], s=50, marker='o', c='red')
 
 
 ax.tick_params(axis='both', labelsize=10)
@@ -365,6 +371,31 @@ ax.set_zlabel('z (um)', fontsize='18')
 # fig.colorbar(p, ax=ax)
 pyplot.show()
 
+
+#%% RPD
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import pyplot
+
+sd_lw_speed['PARTICLE'] = np.ones(len(sd_lw_speed))
+
+epsilon = 0.5
+angle_threshold = 45
+ids, rec, angles = get_angles_rdp(sd_lw_speed, epsilon, angle_threshold)
+ 
+
+plt.figure(figsize=(9, 4.5))
+ax0 = plt.subplot2grid((6, 6), (0, 0), 6, 3, projection='3d')
+ax0.set_facecolor('none')
+ax0.plot(sd_lw_speed.X, sd_lw_speed.Y, sd_lw_speed.Z, 'b-', label='original data')
+ax0.plot(sd_lw_speed.X[ids], sd_lw_speed.Y[ids], sd_lw_speed.Z[ids], 'ro', label='original data')
+ax0.set_title('Smoothed track')
+
+ax1 = plt.subplot2grid((6, 6), (0, 3), 6, 3, projection='3d')
+ax1.set_facecolor('none')
+ax1.plot(rec[:, 0], rec[:, 1], rec[:, 2], 'b-', label='RDP reconstructed eps = '+np.str(epsilon))
+ax1.plot(sd_lw_speed.X.values[ids], sd_lw_speed.Y.values[ids], sd_lw_speed.Z.values[ids], 'ro', label='Turns')
+ax1.set_title('RDP Simplification')
+ax1.legend(loc='best')
 
 
 #%%
@@ -379,4 +410,3 @@ def plotly_scatter(tracks):
     # iplot(fig)
 
 plotly_scatter(sd)
-
